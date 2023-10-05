0d53973785306d23499325f732d11eff9cebb0a3
Ali-Fattahian <ali.fattahian1378@gmail.com>
2023-10-06 00:53:24 +0330

    create rating model and serializer and add it to admin

--- apps/ratings/admin.py
+++ apps/ratings/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Rating
+
+
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ["rate_user", "agent", "rating"]
+
+
+admin.site.register(Rating, RatingAdmin)
---
+++ apps/ratings/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import Rating
+
+
+class RatingSerializer(serializers.ModelSerializer):
+    rate_user = serializers.SerializersMethodField(read_only=True)
+    agent = serializers.SerializersMethodField(read_only=True)
+
+    class Meta:
+        model = Rating
+        exclude = ['updated_at', 'pkid']
+
+    def get_rate_user(self, obj):
+        return obj.rate_user.username
+    
+    def get_agent(self, obj):
+        return obj.agent.user.username
+    
+    
