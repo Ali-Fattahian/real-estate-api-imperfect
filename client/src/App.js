f87a69ac000a87aa507f43be70b7c12a7962f5dd
Ali-Fattahian <ali.fattahian1378@gmail.com>
2023-10-10 22:30:08 +0330

    create react app and delete unused files

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>
+        Real Estate
+        <p>The real test starts here...</p>
+      </h1>
+    </div>
+  );
+}
+
+export default App;
